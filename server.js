b5f3260ea861221b033c9ce1270ae5260f1ec8c1
Anuradha-Agrawal <angel.anuradha29@gmail.com>
2016-11-21 18:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/my-app',function (req, res){
+app.get('/article',function (req, res){
   res.sendFile('my app')  ; 
 });
 app.get('/ui/style.css', function (req, res) {
